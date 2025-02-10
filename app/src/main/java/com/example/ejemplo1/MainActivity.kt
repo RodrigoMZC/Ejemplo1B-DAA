0d74c9676674f4296e08cfbe3ae45b96a546c4c2
RodrigoMZC <rodrigomazuca3@gmail.com>
2025-02-10 13:36:30 -0600

    Modificaciones para la clase del dia 10 de febrero de 2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
@@ -21,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Content()
+                GreetingPreview()
             }
         }
     }
@@ -29,16 +27,22 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun Content() {
-    Text(
-        text = "Hola Jetpack Conpose",
-        color = Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .padding()
+fun GreetingPreview() {
+    Content("Hola", "Raza")
+}
 
-    )
+@Composable
+fun Content(mensaje1: String, mensaje2: String) {
+    Column() {
+        Text(mensaje1,
+            fontSize = 30.sp)
+        Text(mensaje2,
+            fontFamily = FontFamily.Cursive,
+            fontSize = 34.sp)
+        Text("Mazuca",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            lineHeight = 32.sp)
+    }
 }
 
