11ecb808eca1a52df0bfff685ba521d0e5332579
RodrigoMZC <rodrigomazuca3@gmail.com>
2025-02-18 22:16:47 -0600

    Practica 2 LogIn View

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
@@ -20,40 +36,64 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "By MZC",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GreetingPreview()
             }
         }
     }
 }
 
-@Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-
-    Text(
-        text = message,
-        fontSize = 80.sp,
-        lineHeight = 116.sp
-    )
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    Text(
-        text = "Hello $name!",
-        //fontSize = 100.sp,
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Ejemplo1Theme {
-        GreetingText("Feliz Cunpleaños Tec!!")
+        LogIn()
+    }
+}
+
+@Composable
+fun LogIn() {
+    var usr by remember { mutableStateOf(TextFieldValue("")) }
+    var psw by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.database),
+            contentDescription = "Log in img",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+
+
+        TextField(
+            value = usr,
+            onValueChange = { usr = it },
+            label = { Text(text = "Usuario") },
+        )
+        Spacer(modifier =  Modifier.height(8.dp))
+
+        TextField(
+            value = psw,
+            onValueChange = { psw = it },
+            label = { Text(text = "Contraseña") },
+
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .width(270.dp)
+        ) {
+            Text(text = "Iniciar sesion", fontSize = 18.sp)
+        }
+
     }
 }
